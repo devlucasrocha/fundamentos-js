750b82cfa18647a7ea18fee7786898a15ffff246
devlucasrocha <lucasgeorgerocha@gmail.com>
2018-11-22 10:46:29 -0200

    Addint objeto.js

---
+++ objeto.js
@@ -0,0 +1,13 @@
+const prod1 = {}
+prod1.nome = "Celular Ultra Mega"
+prod1.preco = 5881.12
+
+prod1['Desconto legal'] = 0.50 // Evitar atributos com espaco
+
+console.log(prod1)
+
+const prod2 = {
+    nome: 'Camisa Polo',
+    preco: 79.98
+}
+console.log(prod2)
