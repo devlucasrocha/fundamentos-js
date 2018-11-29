16f848458d24f9eef22b9adebebf0755c58f4cad
devlucasrocha <lucasgeorgerocha@gmail.com>
2018-11-29 15:16:47 -0200

    Adding erro.js - Final commit

---
+++ erro.js
@@ -0,0 +1,24 @@
+function tratarErroLancar(erro) {
+    // throw new Error("...")
+    // throw 10
+    // throw true
+    // throw "Mensagem" 
+    throw {
+        nome: erro.name,
+        msg: erro.message,
+        date: new Date
+    }
+}
+
+function imprimirNomeGritando(obj) {
+    try {
+        console.log(obj.name.toUpperCase() + "!!!")
+    } catch (e) {
+        tratarErroLancar(e)
+    } finally {
+        console.log("Final")
+    }
+}
+
+const obj = { nome: "Lucas" }
+imprimirNomeGritando(obj)
